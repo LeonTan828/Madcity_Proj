356f5bec3f760aee2abd5a8e1d4bfdef8c6c0ce0
Leon <yltan828@gmail.com>
2019-06-20 01:04:15 -0500

    added controls

--- main_file.py
+++ main_file.py
@@ -16,18 +16,24 @@ xVals = list(lib_data["X"])
 yVals = list(lib_data["Y"])
 libNames = list(lib_data["LONG_NAME"])
 
-features = folium.FeatureGroup(name= "Social Infrastructures")
+allLib = folium.FeatureGroup(name = "Libraries")
 
 for xVal, yVal, name in zip(xVals, yVals, libNames):
-    features.add_child(folium.CircleMarker(location = [yVal, xVal],
+    allLib.add_child(folium.CircleMarker(location = [yVal, xVal],
     radius = 6, popup = name, fill_color = "red", fill_opacity = 1,
     color = None))
 
-baseMap.add_child(features)
+baseMap.add_child(allLib)
 
 # Add parks as points
 
-features.add_child(folium.GeoJson(data = open("Parks.geojson.json",
+allParks = folium.FeatureGroup(name = "Parks")
+
+allParks.add_child(folium.GeoJson(data = open("Parks.geojson.json",
 "r", encoding = "utf-8-sig").read() ))
+
+baseMap.add_child(allParks)
+
+baseMap.add_child(folium.LayerControl())
 
 baseMap.save("Map1.html")
