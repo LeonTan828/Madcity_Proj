751cd553b239d50addeb83ddcd7d3f3cf091ef31
Leon <yltan828@gmail.com>
2019-06-20 00:49:49 -0500

    small stuff

--- main_file.py
+++ main_file.py
@@ -19,15 +19,15 @@ libNames = list(lib_data["LONG_NAME"])
 features = folium.FeatureGroup(name= "Social Infrastructures")
 
 for xVal, yVal, name in zip(xVals, yVals, libNames):
-    features.add_child(folium.CircleMarker(location = [yVal, xVal], \
-        radius = 6, popup = name, fill_color = "red", fill_opacity = 1, \
-        color = None))
+    features.add_child(folium.CircleMarker(location = [yVal, xVal],
+    radius = 6, popup = name, fill_color = "red", fill_opacity = 1,
+    color = None))
 
 baseMap.add_child(features)
 
 # Add parks as points
 
-features.add_child(folium.GeoJson(data = open("Parks.geojson.json", \
-    "r", encoding = "utf-8-sig").read() ))
+features.add_child(folium.GeoJson(data = open("Parks.geojson.json",
+"r", encoding = "utf-8-sig").read() ))
 
 baseMap.save("Map1.html")
