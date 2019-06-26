785477bf649790343e27fff1234dd717c034e365
Leon <yltan828@gmail.com>
2019-06-26 01:34:17 -0500

    tidying up

--- main_file.py
+++ main_file.py
@@ -23,8 +23,6 @@ for xVal, yVal, name in zip(xVals, yVals, libNames):
     radius = 6, popup = name, fill_color = "green", fill_opacity = 1,
     color = None))
 
-baseMap.add_child(allLib)
-
 # Add parks as points
 
 allParks = folium.FeatureGroup(name = "Parks")
@@ -33,8 +31,6 @@ allParks.add_child(folium.GeoJson(data = open("Parks.geojson.json",
 "r", encoding = "utf-8-sig").read(),
 style_function = lambda x: {"fillColor":"purple"} ))
 
-baseMap.add_child(allParks)
-
 # Add neighborhood
 
 neigh = folium.FeatureGroup(name = "Neighborhood")
@@ -42,8 +38,6 @@ neigh = folium.FeatureGroup(name = "Neighborhood")
 neigh.add_child(folium.GeoJson(data = open("Neighborhood_Associations.geojson.json",
 "r", encoding = "utf-8-sig").read(),
 style_function = lambda x: {"fillColor":"yellow"} ))
-
-baseMap.add_child(neigh)
 
 # Add Incident Report
 
@@ -59,6 +53,10 @@ for xVal, yVal in zip(xVals, yVals):
     radius = 6, fill_color = "red", fill_opacity = 1,
     color = None))
 
+# Adding all map features
+baseMap.add_child(allLib)
+baseMap.add_child(allParks)
+baseMap.add_child(neigh)
 baseMap.add_child(incidents)
 
 baseMap.add_child(folium.LayerControl())
